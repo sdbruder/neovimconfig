490c243a86f5578227bbb67dbead88e9bb182bde
Sergio Bruder <sergio.devojno_bruder@dpdhl.com>
2023-11-07 19:20:54 +0100

    wsl clipboard integration

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,3 +5,18 @@
 local opt = vim.opt
 
 opt.relativenumber = false
+
+vim.cmd([[
+  let g:clipboard = {
+  \   'name': 'WslClipboard',
+  \   'copy': {
+  \      '+': 'clip.exe',
+  \      '*': 'clip.exe',
+  \    },
+  \   'paste': {
+  \      '+': 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+  \      '*': 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+  \   },
+  \   'cache_enabled': 0,
+  \ }
+]])
