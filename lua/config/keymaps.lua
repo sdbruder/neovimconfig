4006dca0565d47ec4ed8eba398c04f3a6d5d64f7
Sergio Bruder <sergio.devojno_bruder@dpdhl.com>
2023-12-21 13:44:13 +0100

    description in keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,8 +3,8 @@
 -- Add any additional keymaps here
 
 -- Cycle between buffers with TAB / Shift-TAB
-vim.keymap.set("n", "<TAB>", ":bnext<cr>")
-vim.keymap.set("n", "<s-TAB>", ":bprevious<cr>")
+vim.keymap.set("n", "<TAB>", ":bnext<cr>", { desc = "Next buffer" })
+vim.keymap.set("n", "<s-TAB>", ":bprevious<cr>", { desc = "Previous buffer" })
 
 -- Move to window using Shift-Arrow keys
 vim.keymap.set("n", "<S-Left>", "<C-w>h", { desc = "Go to left window", remap = true })
