09697d1c7fda171e6ee3ccc8b2aab2c0a20d093d
Sergio Bruder <sergio@bruder.com.br>
2026-06-24 00:45:01 +0200

    updates

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,13 +1,13 @@
 return {
   -- Disable smooth animations
-  { "nvim-mini/mini.animate", enabled = false },
+  -- { "nvim-mini/mini.animate", enabled = false },
 
   -- Disable the distraction-heavy UI notifications
-  { "folke/noice.nvim", enabled = false },
+  -- { "folke/noice.nvim", enabled = false },
 
   -- Disable the popup notifications on the top-right
-  { "rcarriga/nvim-notify", enabled = true },
+  -- { "rcarriga/nvim-notify", enabled = true },
 
   -- Disable dashboard on startup (speeds up initial render)
-  { "folke/persistence.nvim", enabled = false },
+  -- { "folke/persistence.nvim", enabled = false },
 }
